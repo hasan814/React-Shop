20d1246c6adcbf9525c778084ddc77b1f7a9f444
Hasan Moosavi <h.mousavi910@gmail.com>
2025-01-21 19:01:21 +0330

    Added Context Provider for providers

--- src/context/ShopProviders.jsx
+++ src/context/ShopProviders.jsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from "react";
 import { ShopContext } from "./ShopContext";
 
 import PropTypes from "prop-types";
+import toast from "react-hot-toast";
+import api from "../services/config";
 
 const ShopProviders = ({ children }) => {
-  return <ShopContext.Provider value={{}}>{children}</ShopContext.Provider>;
+  // ============ State ============
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // ============ Effect ============
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setLoading(true);
+        const response = await api.get("/products");
+        setProducts(response);
+        toast.success("Products loaded successfully!");
+      } catch (error) {
+        toast.error("Failed to load Products:", error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  // ============ Context Value ============
+  const contextValue = { products, loading };
+
+  // ============ Rendering ============
+  return (
+    <ShopContext.Provider value={contextValue}>{children}</ShopContext.Provider>
+  );
 };
 
 ShopProviders.propTypes = {
