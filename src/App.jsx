dd47fe352196c5ec9b25e958c496850cdaddf644
Hasan Moosavi <h.mousavi910@gmail.com>
2025-01-21 17:36:55 +0330

    Added Router Structures

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { Toaster } from "react-hot-toast";
+import { Outlet } from "react-router-dom";
+
 import ShopProviders from "./context/ShopProviders";
+import Header from "./components/layouts/Header";
+import Footer from "./components/layouts/Footer";
 
 const App = () => {
-  return <ShopProviders>App</ShopProviders>;
+  return (
+    <ShopProviders>
+      <Toaster />
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </ShopProviders>
+  );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { RouterProvider } from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+import router from "./routes";
+
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
