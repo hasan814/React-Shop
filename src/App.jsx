b1fda116d0f558255df1c05aa651d60affa327a0
Hasan Moosavi <h.mousavi910@gmail.com>
2025-01-22 11:05:22 +0330

    Added useReducer Structures

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import { Outlet } from "react-router-dom";
 import ShopProviders from "./context/ShopContext/ShopProviders";
 import Header from "./components/layouts/Header";
 import Footer from "./components/layouts/Footer";
+import CartProvider from "./context/CartContext/CartProvider";
 
 const App = () => {
   return (
     <div className="flex flex-col min-h-screen max-w-[1100px] mx-auto">
       <ShopProviders>
-        <Toaster />
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-6">
-          <Outlet />
-        </main>
-        <Footer />
+        <CartProvider>
+          <Toaster />
+          <Header />
+          <main className="flex-grow container mx-auto px-4 py-6">
+            <Outlet />
+          </main>
+          <Footer />
+        </CartProvider>
       </ShopProviders>
     </div>
   );
---
+++ src/context/CartContext/CartProvider.jsx
@@ -0,0 +1,21 @@
+import { initialState, reducer } from "../../utils/Reducer";
+import { CartContext } from "./CartContext";
+import { useReducer } from "react";
+
+import PropTypes from "prop-types";
+
+const CartProvider = ({ children }) => {
+  // ============ Reducer ============
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // ============ Rendering ============
+  return (
+    <CartContext.Provider value={{ state }}>{children}</CartContext.Provider>
+  );
+};
+
+CartProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default CartProvider;
